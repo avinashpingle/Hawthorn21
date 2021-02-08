a61f96e19ca32421d0e861a933695753420f32dc
avinashpingale <avinashpingale@gmail.com>
2021-02-08 10:12:00 +0530

    Updating pom.xml and keyword class

---
+++ src/main/java/com/testingshastra/uikeywords/Keyword.java
@@ -0,0 +1,16 @@
+package com.testingshastra.uikeywords;
+
+public class Keyword {
+	public void openBrowser(String browser) {
+		switch (browser) {
+		case "Chrome":
+			
+			break;
+		case "Firefox":
+			break;
+		case "IE"
+		break;
+		default:
+		}
+	}
+}
