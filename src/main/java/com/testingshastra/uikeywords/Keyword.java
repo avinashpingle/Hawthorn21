a1bf1e66b2bd9e533dd85cc8db86108dd446ba99
sagarm47 <mahajansagar47@gmail.com>
2021-02-10 11:27:22 +0530

    WebdriverManager Added

--- src/main/java/com/testingshastra/uikeywords/Keyword.java
+++ src/main/java/com/testingshastra/uikeywords/Keyword.java
@@ -1,18 +1,22 @@
 package com.testingshastra.uikeywords;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Keyword {
 	public void openBrowser(String browser) {
 		switch (browser) {
 		case "Chrome":
-			
+				System.out.println("Opening Chrome browser");
+				WebDriverManager.chromedriver().setup();
+				Constant.driver=new ChromeDriver();
 			break;
 		case "Firefox":
 			break;
 		case "IE":
 		break;
 		case "Safari":
-			break;
-		case "Microsoft Edge":
 			break;
 		default:
 		}
