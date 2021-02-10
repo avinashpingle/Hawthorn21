2fc9caf6e7005da25cfe24ef7c28f79573c811b1
VinitaGalande <vinita.galande@gmail.com>
2021-02-10 11:19:38 +0530

    Created Broker Package, BrokerModule Class with add Broker method.

--- src/main/java/com/testingshastra/brokers/BrokerModule.java
+++ src/main/java/com/testingshastra/brokers/BrokerModule.java
@@ -2,7 +2,7 @@ package com.testingshastra.brokers;
 
 public class BrokerModule {
 	private void addBroker() {
-		System.out.println("I'm Adding Broker");
+		System.out.println("I'm Adding Broker.\nIn Masters-> Brokers");
 	}
 public static void main(String[] args) {
 	new BrokerModule().addBroker();
